651f2c911ec70c612340ddc5f8e2f30dd43f11a7
George Secen <george.secen.work@gmail.com>
2024-05-01 16:44:53 -0400

    Added function to clean up the raw fred series data

---
+++ apps/frontend/utils/fred_data.py
@@ -0,0 +1,28 @@
+"""This module helps with all fred series data related functions"""
+
+import pandas as pd
+
+
+def filter_fred_series(series):
+    """Cleans up the fred data series df, removes unwanted data.
+
+    Args:
+        series (df): Pandas dataframe to be cleaned.
+
+    Returns:
+        df: Pandas dataframe with cleaned up data
+    """
+    dates = []
+    values = []
+
+    # Adding needed values to lists
+    for i in series["observations"]:
+        dates.append(i["date"])
+        values.append(i["value"])
+
+    dictionary = {"date": dates, "value": values}
+
+    # Converting dictionary of lists to df
+    df = pd.DataFrame(dictionary)
+
+    return df
