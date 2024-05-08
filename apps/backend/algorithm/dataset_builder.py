a0bd18f4d3bf17d489830985bee1f6c94fc0b0c7
George Secen <george.secen.work@gmail.com>
2024-05-08 15:36:34 -0400

    Added a second ideal dataset idea

--- apps/backend/algorithm/dataset_builder.py
+++ apps/backend/algorithm/dataset_builder.py
@@ -1,7 +1,9 @@
 """This module builds the dataset required to run algorithm"""
 
 from utils.algorithm import get_buy_and_condition_data, sample_algo_request
-from pandas import pd
+from indicators.sma import sma
+
+import pandas as pd
 
 # I want the dataset to consist of the assets and series, followed by that assets or series indicators,
 # followed by the period of that indicator, followed by pandas df of dates and values for that specific
@@ -39,19 +41,57 @@ ideal_dataset = {
     },
 }
 
+# If we just have the asset or series and 1 df for each, searching for the data will be faster
+ideal_dataset2 = {
+    "AAPL": "pandas df containing all required data",
+    "MSFT": "pandas df containing all required data",
+}
+
 
 # Creating an example df so that I dont have to keep requesting the actual data
 # from yahoo finance and fred for testing
-dates = ["2022-01-01", "2022-01-01", "2022-01-01"]
-values = [3, 3.1, 3.2]
+dates = [
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+    "2022-01-01",
+]
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 dictionary = {"date": dates, "value": values}
 example_df = pd.DataFrame(dictionary)
+# print(example_df)
+
+gg = {}
+
+gg["tset"] = sma(4, example_df, "value")
+
+print(gg["tset"])
 
 
 def build_dataset(algorithm):
+    dataset = {}
     required_data = get_buy_and_condition_data(algorithm)
+
+    # Adding the asset/series data to dataset
+    # Adding example_df for now but will be yahoo_finance/fred data once finalized
     for i in required_data:
-        print(i)
+        # Only time 'asset' is not in key is if it is condition of fixed value because in that case
+        # asset does not matter
+
+        # Asset - yahoo finance data
+        # Series - fred series data
+        if "asset" in i:
+            dataset[i["asset"]] = {"ohlcv": example_df}
+
+        if "series" in i:
+            dataset[i["series"]] = {"fred": example_df}
+
+    print(dataset["MSFT"])
 
 
-build_dataset(sample_algo_request)
+# build_dataset(sample_algo_request)
