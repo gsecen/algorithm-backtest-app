18523dd1448343f68b866e36c8458a67279753d0
George Secen <george.secen.work@gmail.com>
2024-05-08 17:18:25 -0400

    Changed sma function to take into account if its yahoo or fred data

--- apps/backend/indicators/sma.py
+++ apps/backend/indicators/sma.py
@@ -3,19 +3,30 @@
 import pandas as pd
 
 
-def sma(period, df, column_name):
+def sma(period, df, metric_type):
     """Calculates and adds simple moving average indicator to dataframe.
 
     Args:
         period (int): Length of sma.
         df (df): Dataframe which you want sma for.
-        column_name (str): Column in dataframe which you want to calculate sma for.
+        metric_type (str): Yahoo finance ("asset") or fred series ("series")
 
     Returns:
         df: Pandas dataframe with new sma data.
     """
+
     # Make sure column does not exist
     if f"sma {period}" not in df.columns:
+
+        # If yahoo finance df calculate based on Close column
+        if metric_type == "asset":
+            column_name = "Close"
+
+        # If fred df calculate based on value column
+        if metric_type == "series":
+            column_name = "value"
+
+        # Adding new column
         df[f"sma {period}"] = df[column_name].rolling(period).mean()
 
     return df
