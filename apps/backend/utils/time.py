24796d77ebeca986941b9837d457bd0c8a1248a0
George Secen <george.secen.work@gmail.com>
2024-05-11 17:10:41 -0400

    Edited the date to epoch function and directly importing datetime from datetime

--- apps/backend/utils/time.py
+++ apps/backend/utils/time.py
@@ -1,6 +1,7 @@
 """This module helps with all time related functions"""
 
-import datetime
+from datetime import datetime
+import pandas as pd
 
 
 def date_to_epoch(date, date_format="%Y-%m-%d"):
@@ -13,12 +14,48 @@ def date_to_epoch(date, date_format="%Y-%m-%d"):
     Returns:
         int: Seconds since epoch.
     """
-    epoch = datetime.datetime(1970, 1, 1)
+    epoch = datetime(1970, 1, 1)
 
     # Converting date string to datetime object
-    datetime_object = datetime.datetime.strptime(date, date_format)
+    datetime_object = datetime.strptime(date, date_format)
 
     # Calculating how many seconds have past since epoch
     diff = datetime_object - epoch
 
     return int(diff.total_seconds())
+
+
+def get_trading_days(start_date, end_date):
+
+    # Gets all SPX data from 1928 until now
+    query_string = f"https://query1.finance.yahoo.com/v7/finance/download/^SPX?period1=-999999999999999&period2=9999999999999999&interval=1d&events=history&includeAdjustedClose=true"
+    df = pd.read_csv(query_string)
+    print(df["Date"])
+    for i in df["Date"]:
+        print(i)
+
+
+# get_trading_days("2020-01-01", "2021-01-01")
+
+
+dates = [
+    "2020-01-01",
+    "2020-01-02",
+    "2020-01-05",
+    "2020-01-06",
+    "2020-01-09",
+    "2020-01-12",
+]
+
+start = "2020-01-04"
+end = "2020-01-11"
+
+
+gg = datetime.datetime.strptime(start, "%Y-%m-%d")
+ff = datetime.datetime.strptime(end, "%Y-%m-%d")
+if gg < ff:
+    print("dsfjio")
+
+print(type(gg))
+
+print(type(54))
