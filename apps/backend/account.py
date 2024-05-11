8babbbf0d5a1de278a735b82d5216c39705b3d32
George Secen <george.secen.work@gmail.com>
2024-05-11 01:26:26 -0400

    Added rough function to calculate the weights each asset has in portfolio

--- apps/backend/account.py
+++ apps/backend/account.py
@@ -46,10 +46,10 @@ example = {
     "2020-01-06": new_holdings,
 }
 
-something = {}
-
 
 def calculate_stock_quantities():
+    something = {}
+
     for date, value in example.items():
         # print(key)
         something[date] = {}
@@ -58,5 +58,33 @@ def calculate_stock_quantities():
             # print(f"{asset} Shares: {shares}")
             something[date][asset] = shares
 
+    return something
 
-print(something)
+
+stock_quantites = {
+    "2020-01-01": {"AAPL": 5000.0, "NVDA": 1000.0},
+    "2020-01-02": {"AAPL": 2500.0, "NVDA": 500.0},
+    "2020-01-03": {"AAPL": 1666.6666666666667, "NVDA": 333.3333333333333},
+    "2020-01-04": {"AAPL": 1250.0, "NVDA": 1250.0},
+    "2020-01-05": {"AAPL": 1000.0, "NVDA": 1000.0},
+    "2020-01-06": {"AAPL": 833.3333333333334, "NVDA": 833.3333333333334},
+}
+
+
+def calculate_portfolio_asset_weights():
+    gg = {}
+
+    for date, value in stock_quantites.items():
+        gg[date] = {}
+        for asset, quantity in value.items():
+            total_stock_value = quantity * prices[asset][date]
+            stock_weight = total_stock_value / balance
+            gg[date][asset] = stock_weight
+
+    return gg
+
+
+print(calculate_portfolio_asset_weights())
+
+
+print(calculate_stock_quantities())
