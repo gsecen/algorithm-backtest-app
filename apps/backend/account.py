f6e2e0ddecc485419899944f5374c7c65ee22df8
George Secen <george.secen.work@gmail.com>
2024-05-16 01:32:05 -0400

    Added a rough function to handle the type buy objects from algorithm

--- apps/backend/account.py
+++ apps/backend/account.py
@@ -61,33 +61,34 @@ def calculate_stock_quantities():
     return something
 
 
-stock_quantites = {
-    "2020-01-01": {"AAPL": 5000.0, "NVDA": 1000.0},
-    "2020-01-02": {"AAPL": 2500.0, "NVDA": 500.0},
-    "2020-01-03": {"AAPL": 1666.6666666666667, "NVDA": 333.3333333333333},
-    "2020-01-04": {"AAPL": 1250.0, "NVDA": 1250.0},
-    "2020-01-05": {"AAPL": 1000.0, "NVDA": 1000.0},
-    "2020-01-06": {"AAPL": 833.3333333333334, "NVDA": 833.3333333333334},
-}
+# stock_quantites = {
+#     "2020-01-01": {"AAPL": 5000.0, "NVDA": 1000.0},
+#     "2020-01-02": {"AAPL": 2500.0, "NVDA": 500.0},
+#     "2020-01-03": {"AAPL": 1666.6666666666667, "NVDA": 333.3333333333333},
+#     "2020-01-04": {"AAPL": 1250.0, "NVDA": 1250.0},
+#     "2020-01-05": {"AAPL": 1000.0, "NVDA": 1000.0},
+#     "2020-01-06": {"AAPL": 833.3333333333334, "NVDA": 833.3333333333334},
+# }
 
 
 def calculate_portfolio_asset_weights():
-    gg = {}
 
-    for date, value in stock_quantites.items():
-        gg[date] = {}
+    weights = {}
+
+    for date, value in calculate_stock_quantities().items():
+        weights[date] = {}
         for asset, quantity in value.items():
             total_stock_value = quantity * prices[asset][date]
             stock_weight = total_stock_value / balance
-            gg[date][asset] = stock_weight
+            weights[date][asset] = stock_weight
 
-    return gg
+    return weights
 
 
 def calculate_portfolio_value():
     gg = {}
 
-    for date, value in stock_quantites.items():
+    for date, value in calculate_stock_quantities().items():
         # gg[date] = {}
         portfolio_value = 0
         for asset, quantity in value.items():
