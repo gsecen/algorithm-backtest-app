4515dde58edb1d3b7f4f62c2414ea165e5d8ecb8
George Secen <george.secen.work@gmail.com>
2024-05-20 16:06:35 -0400

    Added float nan values to the sample df for indicator testing

--- apps/backend/indicators/sample_df.py
+++ apps/backend/indicators/sample_df.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 
 dates = [
+    "2019-12-29",
+    "2019-12-30",
+    "2019-12-31",
     "2020-01-01",
     "2020-01-02",
     "2020-01-03",
@@ -9,11 +12,9 @@ dates = [
     "2020-01-05",
 ]
 
-values = [1, 2, 3, 4, 5]
+values = [float("nan"), float("nan"), float("nan"), 1, 2, 3, 4, 5]
 
 dictionary = {"Date": dates, "value": values}
 
 # Converting dictionary of lists to df
 sample_df = pd.DataFrame(dictionary)
-
-print(sample_df)
