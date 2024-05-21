dd6a9b5326e9cccea4e277a3c455e4038cb9d8d2
George Secen <george.secen.work@gmail.com>
2024-05-21 03:37:47 -0400

    Added a get calmar ratio function to get an assets calmar ratio within date range and added doc comments

---
+++ apps/backend/performance_metrics/calmar_ratio.py
@@ -0,0 +1,24 @@
+from maximum_drawdown import get_maximum_drawdown
+from annualized_return import get_annualized_return
+
+
+def get_calmar_ratio(asset_df, start_date, end_date, column_name="Open"):
+    """Gets an assets calmar ration within date range.
+
+    Args:
+        asset_df (df): Pandas dataframe containing assets data.
+        start_date (str): Start date for calculation.
+        end_date (str): End date for calculation.
+        column_name (str, optional): Column name to calculate value from. Defaults to "Open".
+
+    Returns:
+        float: Assets calmar ratio within date range.
+    """
+
+    # Calculating the calmar ratio
+    annualized_return = get_annualized_return(
+        asset_df, start_date, end_date, column_name
+    )
+    max_drawdown = get_maximum_drawdown(asset_df, start_date, end_date, column_name)
+
+    return annualized_return / max_drawdown
