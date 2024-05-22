9899632087e7a6cfbaf4fef1a68d9195a57b6e79
George Secen <george.secen.work@gmail.com>
2024-05-22 00:06:01 -0400

    Added a calculate return pandas .rolling.apply function and changed everything so that it calculates standard deviation of return as a percentage not price

--- apps/backend/performance_metrics/standard_deviation.py
+++ apps/backend/performance_metrics/standard_deviation.py
@@ -1,15 +1,18 @@
 """This module calculates the standard deviation of an asset"""
 
-from indicators.sample_df import sample_df_v3
+from indicators.sample_df import sample_df_v5
 from utils.time import get_rows_between_dates
 from math import sqrt
 
 
-# Custom .rolling.appy function to calculate the sqaure root of deviation from mean price
-def calculate_sqrt_deviation(data, mean):
+def calculate_return(values):
+    return ((values[1] / values[0]) * 100) - 100
+
+
+def calculate_sqrt_deviation(values, mean):
 
     # Calculating sqaure root of deviation from mean price
-    deviation = data - mean
+    deviation = calculate_return(values) - mean
     squared_deviation = abs(deviation) ** 2
 
     return squared_deviation
@@ -26,14 +29,19 @@ def get_standard_deviation(asset_df, start_date, end_date, column_name="Open"):
         return
     else:
 
-        # Getting the mean of the values
-        mean = asset_df[column_name].mean()
+        # Getting the mean return of the values
+        returns = (
+            asset_df[column_name]
+            .rolling(2)
+            .apply(lambda x: calculate_return(x), raw=True)
+        )
+        mean_returns = returns.mean()
 
         # Getting the squared deviations from mean price
         squared_deviations = (
-            sample_df_v3["value"]
-            .rolling(1)
-            .apply(lambda x: calculate_sqrt_deviation(x, mean), raw=True)
+            asset_df["value"]
+            .rolling(2)
+            .apply(lambda x: calculate_sqrt_deviation(x, mean_returns), raw=True)
         )
 
         # Calculating standard deviation
@@ -45,4 +53,8 @@ def get_standard_deviation(asset_df, start_date, end_date, column_name="Open"):
         return standard_deviation
 
 
-print(get_standard_deviation(sample_df_v3, "2000-01-01", "2050-01-01", "value"))
+# print(sample_df_v5)
+get_standard_deviation(sample_df_v5, "2000-01-01", "2050-01-01", "value")
+
+
+# print(0.7996804917520974 * sqrt(3))
