3d02bae2c741f5b5a02941a69dbc694c5fd735f9
George Secen <george.secen.work@gmail.com>
2024-05-22 01:07:44 -0400

    Added doc comments to calculate return calculate sqrt deviation and get standard deviation functions

--- apps/backend/performance_metrics/standard_deviation.py
+++ apps/backend/performance_metrics/standard_deviation.py
@@ -1,17 +1,36 @@
-"""This module calculates the standard deviation of an asset"""
+"""This module calculates the standard deviation of return of an asset"""
 
-from indicators.sample_df import sample_df_v5
+from indicators.sample_df import sample_df_v4, sample_df_v5
 from utils.time import get_rows_between_dates
 from math import sqrt
 
 
+# Pandas .rolling.appy function
 def calculate_return(values):
+    """Calculates the return as percentage from two values.
+
+    Args:
+        values (list): Two values to calculate percentage from.
+
+    Returns:
+        float: Return as a percentage of the two values.
+    """
     return ((values[1] / values[0]) * 100) - 100
 
 
+# Pandas .rolling.apply function
 def calculate_sqrt_deviation(values, mean):
+    """Calculates the square root of deviation as a percentage from mean return.
 
-    # Calculating sqaure root of deviation from mean price
+    Args:
+        values (list): Two values to calculate square root of deviation as a percentage from.
+        mean (_type_): Dataframes mean return as a percentage.
+
+    Returns:
+        float: Square rooted deviation.
+    """
+
+    # Calculating sqaure root of deviation from mean return
     deviation = calculate_return(values) - mean
     squared_deviation = abs(deviation) ** 2
 
@@ -19,6 +38,17 @@ def calculate_sqrt_deviation(values, mean):
 
 
 def get_standard_deviation(asset_df, start_date, end_date, column_name="Open"):
+    """Gets the standard deviation of return of an asset within date range.
+
+    Args:
+        asset_df (df): Pandas dataframe containing assets data.
+        start_date (str): Start date for calculation.
+        end_date (str): End date for calculation.
+        column_name (str, optional): Column name to calculate value from. Defaults to "Open".
+
+    Returns:
+        float: Assets standard deviation of return within date range.
+    """
     if asset_df is None:
         return
 
@@ -51,10 +81,3 @@ def get_standard_deviation(asset_df, start_date, end_date, column_name="Open"):
         standard_deviation = sqrt(mean_of_devaition_squared)
 
         return standard_deviation
-
-
-# print(sample_df_v5)
-get_standard_deviation(sample_df_v5, "2000-01-01", "2050-01-01", "value")
-
-
-# print(0.7996804917520974 * sqrt(3))
