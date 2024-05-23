4315081685e3efc1b7a2ee307960f385777db371
George Secen <george.secen.work@gmail.com>
2024-05-23 13:14:44 -0400

    Deleted the annualized downside and standard deviation files and added the functions to the downside and standard deviation files

--- apps/backend/performance_metrics/downside_deviation.py
+++ apps/backend/performance_metrics/downside_deviation.py
@@ -2,6 +2,7 @@
 
 from utils.time import get_rows_between_dates
 from statistics import stdev
+from math import sqrt
 
 
 # Pandas .rolling.appy function
@@ -59,6 +60,24 @@ def get_downside_deviation(asset_df, start_date, end_date, column_name="Open"):
         return stdev(negative_returns)
 
 
-from indicators.sample_df import sample_df_aaple
+def get_annualized_downside_deviation(
+    asset_df, start_date, end_date, column_name="Open"
+):
+    """Gets the annualized downside deviation of return of an asset within date range.
 
-print(get_downside_deviation(sample_df_aaple, "2000-01-01", "2050-01-01", "value"))
+    Args:
+        asset_df (df): Pandas dataframe containing assets data.
+        start_date (str): Start date for calculation.
+        end_date (str): End date for calculation.
+        column_name (str, optional): Column name to calculate value from. Defaults to "Open".
+
+    Returns:
+        float: Assets annualized downside deviation of return within date range.
+    """
+
+    # Calculate annualized downside deviation
+    annualized_downside_deviation = get_downside_deviation(
+        asset_df, start_date, end_date, column_name
+    ) * sqrt(365.25)
+
+    return annualized_downside_deviation
--- apps/backend/performance_metrics/standard_deviation.py
+++ apps/backend/performance_metrics/standard_deviation.py
@@ -2,6 +2,7 @@
 
 from utils.time import get_rows_between_dates
 from statistics import stdev
+from math import sqrt
 
 
 # Pandas .rolling.appy function
@@ -49,3 +50,26 @@ def get_standard_deviation(asset_df, start_date, end_date, column_name="Open"):
         # Getting standard deviation
         # Doing returns[1:] becuase first value in returns always nan
         return stdev(returns[1:])
+
+
+def get_annualized_standard_deviation(
+    asset_df, start_date, end_date, column_name="Open"
+):
+    """Gets the annualized standard deviation of return of an asset within date range.
+
+    Args:
+        asset_df (df): Pandas dataframe containing assets data.
+        start_date (str): Start date for calculation.
+        end_date (str): End date for calculation.
+        column_name (str, optional): Column name to calculate value from. Defaults to "Open".
+
+    Returns:
+        float: Assets annualized standard deviation of return within date range.
+    """
+
+    # Calculate annualized standard deviation
+    annualized_standard_deviation = get_standard_deviation(
+        asset_df, start_date, end_date, column_name
+    ) * sqrt(365.25)
+
+    return annualized_standard_deviation
