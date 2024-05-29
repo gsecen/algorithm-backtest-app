a11064680088e54e7c78f8c33c979ccb0a4b351c
George Secen <george.secen.work@gmail.com>
2024-05-29 19:35:40 -0400

    Updated import name to get historical cumulative returns

--- apps/backend/performance_metrics/__init__.py
+++ apps/backend/performance_metrics/__init__.py
@@ -1,7 +1,9 @@
 from performance_metrics.annualized_return import get_annualized_return
 from performance_metrics.calmar_ratio import get_calmar_ratio
 from performance_metrics.downside_deviation import get_downside_deviation
-from performance_metrics.historical_returns import get_historical_returns
+from performance_metrics.historical_cumulative_returns import (
+    get_historical_cumulative_returns,
+)
 from performance_metrics.maximum_drawdown import get_maximum_drawdown
 from performance_metrics.sharp_ratio import get_sharp_ratio
 from performance_metrics.sortino_ratio import get_sortino_ratio
