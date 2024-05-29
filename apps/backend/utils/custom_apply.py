6fb777b64d1caa92724a29aeeaf963bc6b970f23
George Secen <george.secen.work@gmail.com>
2024-05-29 19:19:08 -0400

    Added calculate cumulative return function which calculates the cumulative return between current value and starting value and added doc comments

--- apps/backend/utils/custom_apply.py
+++ apps/backend/utils/custom_apply.py
@@ -29,3 +29,17 @@ def calculate_negative_return(values):
         return float("nan")
     else:
         return return_percentage
+
+
+def calculate_cumulative_return(current_value, starting_value):
+    """Calculates the cumulative return between current value and starting value.
+
+    Args:
+        current_value (list): List of 1 value.
+        (List of 1 value because values from .rolling.apply are in list form)
+        starting_value (float): Starting value.
+
+    Returns:
+        float: Cumulative return as a percentage between current and starting value.
+    """
+    return ((current_value[0] - starting_value) / starting_value) * 100
