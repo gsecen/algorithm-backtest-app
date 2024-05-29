b5bfbe9c88b34ec6c3334a78dd58d817a6bd8242
George Secen <george.secen.work@gmail.com>
2024-05-29 19:58:34 -0400

    Added pearsons correlation function

--- apps/backend/comparative_metrics/__init__.py
+++ apps/backend/comparative_metrics/__init__.py
@@ -1,4 +1,5 @@
 from comparative_metrics.beta import get_beta
 from comparative_metrics.coefficient_determination import get_coefficient_determination
 from comparative_metrics.jensens_alpha import get_jensens_alpha
+from comparative_metrics.pearson_correlation import get_pearson_correlation
 from comparative_metrics.treynor_ratio import get_treynor_ratio
