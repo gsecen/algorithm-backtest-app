f0260698170e5a88c4348f9552ebdc00aa07a4b8
George Secen <george.secen.work@gmail.com>
2024-05-30 00:59:36 -0400

    Added get comparative metrics function which gets all the comparative metrics of an asset or portfolio compared to another asset or portfolio with doc comments, also added get all metrics function which gets all the comparative and perfomance metrics of all benchmarks and portfolio with doc comments

--- apps/backend/backtest_metrics.py
+++ apps/backend/backtest_metrics.py
@@ -1,13 +1,16 @@
-import performance_metrics
+import performance_metrics as pm
+import comparative_metrics as cm
 from utils.dataframe import build_dataframe_from_tuples
 from functools import partial
 
 
 class BacktestMetrics:
 
-    def __init__(self, algorithm, portfolio_data):
+    def __init__(self, algorithm, portfolio_data, dataset):
         self.benchmarks = algorithm["benchmarks"]
+        self.algorithm_name = algorithm["name"]
         self.portfolio_value_data = portfolio_data["portfolio_values"]
+        self.dataset = dataset
 
         self.traded_dates = list(self.portfolio_value_data.keys())
         self.first_traded_date = self.traded_dates[0]
@@ -16,24 +19,28 @@ class BacktestMetrics:
         self.portfolio_dataframe = self.build_portfolio_dataframe()
 
         self.performance_metrics = {
-            "annualized_return": performance_metrics.get_annualized_return,
-            "calmar_ratio": performance_metrics.get_calmar_ratio,
-            "downside_deviation": performance_metrics.get_downside_deviation,
-            "historical_cumulative_returns": performance_metrics.get_historical_cumulative_returns,
-            "maximum_drawdown": performance_metrics.get_maximum_drawdown,
-            "sharp_ratio": performance_metrics.get_sharp_ratio,
-            "sortino_ratio": performance_metrics.get_sortino_ratio,
-            "standard_deviation": performance_metrics.get_standard_deviation,
-            "total_cumulative_return": performance_metrics.get_total_cumulative_return,
-            "trailing_return_30day": partial(
-                performance_metrics.get_trailing_return, days=30
-            ),
-            "trailing_return_60day": partial(
-                performance_metrics.get_trailing_return, days=60
-            ),
-            "trailing_return_90day": partial(
-                performance_metrics.get_trailing_return, days=90
-            ),
+            "annualized_return": pm.get_annualized_return,
+            "calmar_ratio": pm.get_calmar_ratio,
+            "downside_deviation": pm.get_downside_deviation,
+            "annualized_downside_deviation": pm.get_annualized_downside_deviation,
+            # "historical_cumulative_returns": pm.get_historical_cumulative_returns,
+            "maximum_drawdown": pm.get_maximum_drawdown,
+            "sharp_ratio": pm.get_sharp_ratio,
+            "sortino_ratio": pm.get_sortino_ratio,
+            "standard_deviation": pm.get_standard_deviation,
+            "annualized_standard_deviation": pm.get_annualized_standard_deviation,
+            "total_cumulative_return": pm.get_total_cumulative_return,
+            "trailing_return_30day": partial(pm.get_trailing_return, days=30),
+            "trailing_return_60day": partial(pm.get_trailing_return, days=60),
+            "trailing_return_90day": partial(pm.get_trailing_return, days=90),
+        }
+
+        self.comparative_metrics = {
+            "beta": cm.get_beta,
+            "coefficient_determination": cm.get_coefficient_determination,
+            # "jenses_alpha": cm.get_jensens_alpha,
+            "pearson_correlation": cm.get_pearson_correlation,
+            "treynor_ratio": cm.get_treynor_ratio,
         }
 
     def build_portfolio_dataframe(self):
@@ -59,17 +66,77 @@ class BacktestMetrics:
         """Gets all the performance metrics of an asset or portfolio.
 
         Args:
-            dataframe (df): Pandas dataframe.
+            dataframe (df): Pandas dataframe containing assets or portfolios data.
 
         Returns:
             dict: Dictionary with performance metric names as keys, and performance
             metric values as values.
         """
+        if dataframe is None:
+            return None
+
         metrics = {}
 
         for metric_name, metric_function in self.performance_metrics.items():
             metrics[metric_name] = metric_function(
                 dataframe, self.first_traded_date, self.last_traded_date
             )
+
+        return metrics
+
+    def get_comparative_metrics(self, dataframe1, dataframe2):
+        """Gets all the comparative metrics of an asset or portfolio compared to another asset or portfolio.
+
+        Args:
+            dataframe1 (df): Pandas dataframe containing first assets or portfolios data.
+            dataframe2 (df): Pandas dataframe containing second assets or portfolios data.
+
+        Returns:
+            dict: Dictionary of performance metric names as keys and their values as values.
+        """
+        if dataframe1 is None or dataframe2 is None:
+            return None
+
+        metrics = {}
+
+        for metric_name, metric_function in self.comparative_metrics.items():
+            metrics[metric_name] = metric_function(
+                dataframe1, dataframe2, self.first_traded_date, self.last_traded_date
+            )
+
+        return metrics
+
+    def get_all_metrics(self):
+        """Gets all the comparative and performance metrics of all benchmarks and portfolio.
+
+        Returns:
+            dict: Dictionary of performance metric names as keys and their values as values, dictionary
+            of comparative metric names as keys and their values as values.
+        """
+        metrics = {}
+        metrics["performance_metrics"] = {}
+        metrics["comparative_metrics"] = {}
+
+        # Getting and adding algorithm performance metrics
+        algorithm_performance_metrics = self.get_performance_metrics(
+            self.portfolio_dataframe
+        )
+        metrics["performance_metrics"][
+            self.algorithm_name
+        ] = algorithm_performance_metrics
+
+        # Getting all the benchmark metrics
+        for asset in self.benchmarks:
+            asset_df = self.dataset[asset]
+
+            # Getting and adding performance metrics
+            asset_performance_metrics = self.get_performance_metrics(asset_df)
+            metrics["performance_metrics"][asset] = asset_performance_metrics
+
+            # Getting and adding comparative metrics
+            asset_comparative_metrics = self.get_comparative_metrics(
+                self.portfolio_dataframe, asset_df
+            )
+            metrics["comparative_metrics"][asset] = asset_comparative_metrics
 
         return metrics
