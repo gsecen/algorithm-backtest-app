dc794f499b0b481dc835f419a8263f8d3b5427e8
George Secen <george.secen.work@gmail.com>
2024-05-29 20:23:35 -0400

    Added get annualized standard deviation and get annualized downside deviation

--- apps/backend/performance_metrics/__init__.py
+++ apps/backend/performance_metrics/__init__.py
@@ -1,12 +1,18 @@
 from performance_metrics.annualized_return import get_annualized_return
 from performance_metrics.calmar_ratio import get_calmar_ratio
-from performance_metrics.downside_deviation import get_downside_deviation
+from performance_metrics.downside_deviation import (
+    get_downside_deviation,
+    get_annualized_downside_deviation,
+)
 from performance_metrics.historical_cumulative_returns import (
     get_historical_cumulative_returns,
 )
 from performance_metrics.maximum_drawdown import get_maximum_drawdown
 from performance_metrics.sharp_ratio import get_sharp_ratio
 from performance_metrics.sortino_ratio import get_sortino_ratio
-from performance_metrics.standard_deviation import get_standard_deviation
+from performance_metrics.standard_deviation import (
+    get_standard_deviation,
+    get_annualized_standard_deviation,
+)
 from performance_metrics.total_cumulative_return import get_total_cumulative_return
 from performance_metrics.trailing_return import get_trailing_return
