8b81e82bf8aaf4bcac65f577c910c35d4ee0720d
George Secen <george.secen.work@gmail.com>
2024-05-31 01:35:29 -0400

    Added some more sample dataframes which are actual asset values from tradingview

--- apps/backend/indicators/sample_df.py
+++ apps/backend/indicators/sample_df.py
@@ -50,10 +50,26 @@ dates5 = [
     "2020-01-04",
 ]
 
+aapl_dates = [
+    "2023-02-14",
+    "2023-02-15",
+    "2023-02-16",
+    "2023-02-17",
+]
+
+spy_dates = [
+    "2023-02-14",
+    "2023-02-15",
+    "2023-02-16",
+    "2023-02-17",
+]
+
 values1 = [float("nan"), float("nan"), float("nan"), 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 values2 = [2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 values3 = [53.73, 53.87, 53.85, 53.88, 54.08, 54.14, 54.50, 54.30, 54.40, 54.16]
 values4 = [182.40, 182.74, 184.57, 183.05]
+aapl_values = [153.20, 155.33, 153.71, 152.55]
+spy_values = [412.64, 413.98, 408.28, 407.26]
 values5 = [100, 120, 105, 110, 125, 130, 140, 135, 125, 130]
 
 dictionary = {"Date": dates, "value": values1}
@@ -61,6 +77,8 @@ dictionary2 = {"Date": dates, "value": values2}
 dictionary3 = {"Date": dates2, "value": values3}
 dictionary4 = {"Date": dates5, "value": values4}
 dictionary5 = {"Date": dates4, "value": values5}
+dictionary6 = {"Date": aapl_dates, "value": aapl_values}
+dictionary7 = {"Date": spy_dates, "value": spy_values}
 
 # Converting dictionary of lists to df
 sample_df = pd.DataFrame(dictionary)
@@ -69,3 +87,6 @@ sample_df_no_nans = pd.DataFrame(dictionary2)
 sample_df_v3 = pd.DataFrame(dictionary3)
 sample_df_v4 = pd.DataFrame(dictionary4)
 sample_df_v5 = pd.DataFrame(dictionary5)
+
+sample_df_aaple = pd.DataFrame(dictionary6)
+sample_df_spy = pd.DataFrame(dictionary7)
