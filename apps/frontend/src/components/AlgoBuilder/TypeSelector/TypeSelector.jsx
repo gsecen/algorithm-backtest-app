d907730affd540c37eb986bb884f00181e238885
George Secen <george.secen.work@gmail.com>
2024-06-16 17:34:22 -0400

    Added rough functionality to the type selector menu component

--- apps/frontend/src/components/AlgoBuilder/TypeSelector/TypeSelector.jsx
+++ apps/frontend/src/components/AlgoBuilder/TypeSelector/TypeSelector.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./typeSelector.css";
 
-const TypeSelector = () => {
-  return <div>TypeSelector</div>;
+const TypeSelector = (props) => {
+  return (
+    <div>
+      Add
+      <ul>
+        <li>
+          <button onClick={props.addBuy}>Asset</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default TypeSelector;
