91e7b17d9401e53fbd2d1ecc77491dec48148c69
George Secen <george.secen.work@gmail.com>
2024-06-24 20:02:07 -0400

    Added comments to function and doc comment to component

--- apps/frontend/src/components/AlgoBuilder/AlgoTypes/Buy/Buy.jsx
+++ apps/frontend/src/components/AlgoBuilder/AlgoTypes/Buy/Buy.jsx
@@ -4,17 +4,31 @@ import "./buy.css";
 import EditDelete from "../../../AlgoBuilder/EditDelete/EditDelete";
 import Exclamtion from "../../../Exclamation/Exclamtion";
 
-const Buy = () => {
-  const [tickerSymbol, setTickerSymbol] = useState("TICKER");
+/**
+ *
+ * @property {function} deleteMe The function from parent which will element.
+ * @property {function} updateTickerSymbol The function from parent which will update elements ticker symbol.
+ * @property {int} id The elements unqiue id.
+ * @property {string} tickerSymbol The ticker symbol which will be displayed to the user.
+ * @returns {ReactNode} Algorithms type buy react element.
+ */
+const Buy = (props) => {
+  const [tickerSymbol, setTickerSymbol] = useState(props.tickerSymbol);
   const [editableInput, setEditableInput] = useState(false);
   const [editDeleteMenu, setEditDeleteMenu] = useState("hidden");
   const [hovering, setHovering] = useState(false);
 
+  // Variables which will be passed down to the edit delete menu
+  const myId = props.id;
+  const deleteMeFunction = props.deleteMe;
+
+  // Edit delete menu is shown when buy item is hovered or input is being edited
   let editDeleteMenuOpacity = 1;
   if (editDeleteMenu === "hidden") {
     editDeleteMenuOpacity = 0;
   }
 
+  // Exclamation is shown if ticker symbol is "TICKER" which means it still needs to be edited
   let exclamationVisibility = "hidden";
   let exclamationOpacity = 0;
   if (tickerSymbol === "TICKER") {
@@ -38,30 +52,33 @@ const Buy = () => {
     }
   }
 
+  // When the input is done being edited checks to see if still hovering over buy item
   function maybeHideEditDeleteMenu() {
     if (!hovering) {
       setEditDeleteMenu("hidden");
     }
   }
 
+  // Used when the user wants to edit ticker symbol
   const Input = (
     <input
       onChange={(e) => {
-        setTickerSymbol(e.target.value);
+        setTickerSymbol(e.target.value.toUpperCase());
       }}
       // When input loses focus (clicked outside of input)
       onBlur={() => {
         toggleEditable();
         maybeHideEditDeleteMenu();
+        props.updateTickerSymbol(props.id, tickerSymbol);
       }}
       className="ticker-symbol-input"
       autoFocus={true}
       value={tickerSymbol}
-      // maxLength={20}
       type="text"
     />
   );
 
+  // Used when the user is not trying to edit input
   const P = <p className="ticker-symbol-text">{tickerSymbol}</p>;
 
   return (
@@ -80,6 +97,8 @@ const Buy = () => {
           visibility: editDeleteMenu,
           opacity: editDeleteMenuOpacity,
         }}
+        id={myId}
+        deleteMe={deleteMeFunction}
         editMe={toggleEditable}
         className="edit-delete-item"
       ></EditDelete>
