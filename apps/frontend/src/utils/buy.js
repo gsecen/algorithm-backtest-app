c2813785fb4a98664edbe310329220d548c8b318
George Secen <george.secen.work@gmail.com>
2024-07-08 00:37:08 -0400

    Added cloneBuyComponent with doc comments to clone a type buy react component with updated data such as functions.

--- apps/frontend/src/utils/buy.js
+++ apps/frontend/src/utils/buy.js
@@ -1,4 +1,5 @@
 // This file contains functions which help with the buy component
+import React from "react";
 import Buy from "../components/AlgoBuilder/AlgoTypes/Buy/Buy";
 
 /**
@@ -13,11 +14,37 @@ export const createBuyComponent = (deleteMeFunction, updateTickerFunction) => {
     <Buy
       key={randomNumber}
       id={randomNumber}
+      ticker={"TICKER"}
       deleteMe={deleteMeFunction}
       updateTickerSymbol={updateTickerFunction}
-      ticker={"TICKER"}
     ></Buy>
   );
+
+  return Component;
+};
+
+/**
+ * Clones a algo type buy react component with updated data such as functions.
+ * @param {*} BuyComponent Algo type buy component.
+ * @param {function} deleteMeFunction Function used so that buy component can delete itself.
+ * @param {function} updateTickerFunction Function used to that buy component can update ticker symbol.
+ * @returns {<ReactElement>} Algo type buy component.
+ */
+export const cloneBuyComponent = (
+  BuyComponent,
+  deleteMeFunction,
+  updateTickerFunction
+) => {
+  const id = BuyComponent.props.id;
+  const ticker = BuyComponent.props.ticker;
+
+  const Component = React.cloneElement(<Buy></Buy>, {
+    key: id,
+    id: id,
+    ticker: ticker,
+    deleteMe: deleteMeFunction,
+    updateTickerSymbol: updateTickerFunction,
+  });
 
   return Component;
 };
