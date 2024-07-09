1ea8e2fb4adf7e8a0a04a8c87269702af7589110
George Secen <george.secen.work@gmail.com>
2024-07-09 19:29:59 -0400

    Added updateSpecifiedWeightComponentWeight function with doc comments which changes specified weight of specified weight component with id.

--- apps/frontend/src/utils/specifiedWeight.js
+++ apps/frontend/src/utils/specifiedWeight.js
@@ -3,7 +3,7 @@ import React from "react";
 import SpecifiedWeight from "../components/AlgoBuilder/AlgoTypes/SpecifiedWeight/SpecifiedWeight";
 /**
  * Creates a algo type specified weight component
- * @param {function} deleteMeFunction Function used so that buy component can delete itself.
+ * @param {function} deleteMeFunction Function used so that specified weight component can delete itself.
  * @param {function} updateTickerFunction Function used to that specified weight component can update weight.
  * @returns {<ReactElement>} Algo type specified weight component.
  */
@@ -28,7 +28,7 @@ export const createSpecifiedWeightComponent = (
 /**
  * Will create a specified weight component for each task when weighting type changes to specified.
  * @param {Array.<ReactElement>} items The list of react elements you need to create specified weights for.
- * @param {function} deleteMeFunction Function used so that buy component can delete itself.
+ * @param {function} deleteMeFunction Function used so that specified weight component can delete itself.
  * @param {function} updateWeightFunction Function used to that specified weight component can update weight.
  * @returns {Array.<ReactElement>} List of specified weight components.
  */
@@ -53,7 +53,7 @@ export const buildSpecifiedWeights = (
 /**
  * Clones a algo type specified weight component with updated data such as functions.
  * @param {<ReactElement>} SpecifiedWeightComponent
- * @param {function} deleteMeFunction Function used so that buy component can delete itself.
+ * @param {function} deleteMeFunction Function used so that specified weight component can delete itself.
  * @param {function} updateWeightFunction Function used to that specified weight component can update weight.
  * @returns {<ReactElement>} Algo type specified weight component.
  */
@@ -74,4 +74,45 @@ export const cloneSpecifiedWeightComponent = (
   });
 
   return Component;
+};
+
+/**
+ * Changes specified weight of specified weight component with id.
+ * @param {int} id The prop id of the specified weight component whos specified weight will be changed.
+ * @param {string} specifiedWeight The specified weight to update specified components specified weight to.
+ * @param {Array.<ReactElement>} items The list of react elements from which the specified weight component is in.
+ * @param {function} deleteMeFunction Function used so that specified weight component can delete itself.
+ * @param {function} updateWeightFunction Function used to that specified weight component can update weight.
+ * @returns {Array.<ReactElement>} List of react elements with updated specified weight component.
+ */
+export const updateSpecifiedWeightComponentWeight = (
+  id,
+  specifiedWeight,
+  items,
+  deleteMeFunction,
+  updateWeightFunction
+) => {
+  // Search for item with specified id
+  items.forEach((item, index) => {
+    if (item.props.id === id) {
+      const id = item.props.id;
+
+      // Clone component with specified weight
+      const Component = React.cloneElement(
+        <SpecifiedWeight></SpecifiedWeight>,
+        {
+          id: id,
+          key: id,
+          specifiedWeight: specifiedWeight,
+          deleteMe: deleteMeFunction,
+          updateSpecifiedWeight: updateWeightFunction,
+        }
+      );
+
+      // Replacing old component with clone
+      items[index] = Component;
+    }
+  });
+
+  return items;
 };
