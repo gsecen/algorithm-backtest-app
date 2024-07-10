36de3b81485e1b33ea2048b15b70b3b6dba1b110
George Secen <george.secen.work@gmail.com>
2024-07-10 00:25:53 -0400

    Added spliceListsByIdAtSameIndex function with doc comments which finds react element in firsList with id, removes it, removes react element in secondList that has same index as item which was removed in firstList

--- apps/frontend/src/utils/instructions.js
+++ apps/frontend/src/utils/instructions.js
@@ -17,3 +17,23 @@ export const deleteItemById = (id, items) => {
 
   return items;
 };
+
+/**
+ * Finds react element in firsList with id, removes it, removes react element in secondList that has same index as item which was removed in firstList.
+ * @param {int} id The react elements id prop value which you want to search for in firstList.
+ * @param {Array.<ReactElement>} firstList The list of react elements from which you search for with id and remove.
+ * @param {Array.<ReactElement>} secondList The list of react elements from which you will also remove element from which will be same index as element removed from firstList.
+ * @returns {[Array.<ReactElement>, Array.<ReactElement>]} firstList without react element with id, secondList without element that has same index as element removed in firstList.
+ */
+export const spliceListsByIdAtSameIndex = (id, firstList, secondList) => {
+  // Removing the specified item by id from first list and its corresponding item (same index) from secondList
+  firstList.forEach((item, index) => {
+    if (item.props.id === id) {
+      // Removing items from each of the lists
+      firstList.splice(index, 1);
+      secondList.splice(index, 1);
+    }
+  });
+
+  return [firstList, secondList];
+};
