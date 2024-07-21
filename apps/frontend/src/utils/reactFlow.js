d928b64f64915c28d4c12c35db9a4be380c1e17e
George Secen <george.secen.work@gmail.com>
2024-07-21 03:42:57 -0400

    Added setHiddenAllNodeChildren function which sets the hidden attribute in all the nodes children to true or false and added doc comments

--- apps/frontend/src/utils/reactFlow.js
+++ apps/frontend/src/utils/reactFlow.js
@@ -1,6 +1,6 @@
 // This file contains functions which help with the react flow nodes
 
-import { getOutgoers } from "@xyflow/react";
+import { getOutgoers, applyNodeChanges } from "@xyflow/react";
 
 /**
  * Gets all the immediate children of a node.
@@ -62,4 +62,35 @@ export const deleteNodeAndAllNodeChildren = (
   // Will be the deleteElements method from useReactFlow hook to delete nodes from the react flow state
   // https://reactflow.dev/api-reference/types/react-flow-instance#deleteelements
   deleteElementsFunction({ nodes: children });
+};
+
+/**
+ * Sets the hidden attribute in all the nodes children to true or false.
+ * @param {boolean} hidden What you want the hidden attribute to be set to.
+ * @param {string} id Id of the node you want to set hidden attribute in all of its children.
+ * @param {Array.<ReactFlowNode>} nodes List of all react flow nodes.
+ * @param {Array.<ReactFlowEdge>} edges List of all react flow edges.
+ * @param {function} setNodesFunction setNodes method from useNodesState hook.
+ */
+export const setHiddenAllNodeChildren = (
+  hidden,
+  id,
+  nodes,
+  edges,
+  setNodesFunction
+) => {
+  // Get all children of node
+  const children = getAllNodeChildren(id, nodes, edges);
+
+  // Set hidden property to true or false in all children nodes
+  children.forEach((node) => {
+    node.hidden = hidden;
+  });
+
+  // Applies the node changes to the nodes in nodes
+  // https://reactflow.dev/api-reference/utils/apply-node-changes
+  const newNodes = applyNodeChanges(children, nodes);
+
+  // Updating the nodes in the react flow state
+  setNodesFunction(newNodes);
 };
