849755054a7a5ab6b89295e357fca94f06ae621d
George Secen <george.secen.work@gmail.com>
2024-07-27 02:17:46 -0400

    Updated the app to include algo builder and removed strict mode as it was causing useeffect to render twice and re add nodes it already added

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,5 +1,18 @@
+import AlgoBuilder from "./components/AlgoBuilder/AlgoBuilder";
+import "./app.css";
+
+// Like useContext for the react flow
+// https://reactflow.dev/api-reference/react-flow-provider
+import { ReactFlowProvider } from "@xyflow/react";
+
 function App() {
-  return <div>app</div>;
+  return (
+    <div>
+      <ReactFlowProvider>
+        <AlgoBuilder></AlgoBuilder>
+      </ReactFlowProvider>
+    </div>
+  );
 }
 
 export default App;
