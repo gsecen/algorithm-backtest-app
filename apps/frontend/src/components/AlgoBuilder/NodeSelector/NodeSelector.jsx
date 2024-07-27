c758fe55c2ce2edb0c970bb2524bb7d4ae7aace3
George Secen <george.secen.work@gmail.com>
2024-07-27 02:35:48 -0400

    Added rough functionality to replace itself with the desired node selected

--- apps/frontend/src/components/AlgoBuilder/NodeSelector/NodeSelector.jsx
+++ apps/frontend/src/components/AlgoBuilder/NodeSelector/NodeSelector.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
-import { Handle, Position } from "@xyflow/react";
+import { useState, useRef } from "react";
+import { Handle, Position, useReactFlow, useNodeId } from "@xyflow/react";
+import { replaceNode } from "../../../utils/reactFlow";
 import "./nodeSelector.css";
 
 const NodeSelector = ({ data }) => {
   const [showDropdown, setShowDropdown] = useState(true);
+  const myId = useRef(useNodeId());
+
+  const { getNode, getNodes, setNodes } = useReactFlow();
+
+  function test() {
+    console.log(myId);
+    replaceNode(myId.current, "buy", getNodes(), getNode, setNodes);
+  }
+
+  function test2() {
+    replaceNode(myId.current, "expression", getNodes(), getNode, setNodes);
+  }
 
   return (
     <div>
@@ -17,8 +30,12 @@ const NodeSelector = ({ data }) => {
         add asset, weight, if else... click for dropdown
       </div>
       <ul className={`node-selector-list ${showDropdown ? "" : "hidden"}`}>
-        <li className="node-selector-list-item">Asset</li>
-        <li className="node-selector-list-item">If Else</li>
+        <li onClick={test} className="node-selector-list-item">
+          Asset
+        </li>
+        <li onClick={test2} className="node-selector-list-item">
+          If Else
+        </li>
         <li className="node-selector-list-item">Group</li>
         <li className="node-selector-list-item">Weight</li>
         <li className="node-selector-list-item">Paste</li>
