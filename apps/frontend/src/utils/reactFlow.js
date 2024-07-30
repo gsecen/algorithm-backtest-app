79cfdf0b0b60800464938e14e47463c9a1a3a9cc
George Secen <george.secen.work@gmail.com>
2024-07-30 01:30:00 -0400

    Added deleteAllNodeChildren function with doc comments which deletes all children of a node

--- apps/frontend/src/utils/reactFlow.js
+++ apps/frontend/src/utils/reactFlow.js
@@ -70,6 +70,28 @@ export const deleteNodeAndAllNodeChildren = (
 };
 
 /**
+ * Deletes all children of node.
+ * @param {string} id Id of the node you want to delete all of its children.
+ * @param {Array.<ReactFlowNode>} nodes List of all react flow nodes.
+ * @param {Array.<ReactFlowEdge>} edges List of all react flow edges.
+ * @param {function} deleteElementsFunction deleteElements method from useReactFlow hook.
+ */
+export const deleteAllNodeChildren = (
+  id,
+  nodes,
+  edges,
+  deleteElementsFunction
+) => {
+  // Get all children of node
+  const children = getAllNodeChildren(id, nodes, edges);
+
+  // Delete elements from the react flow
+  // Will be the deleteElements method from useReactFlow hook to delete nodes from the react flow state
+  // https://reactflow.dev/api-reference/types/react-flow-instance#deleteelements
+  deleteElementsFunction({ nodes: children });
+};
+
+/**
  * Sets the hidden attribute in all the nodes children to true or false.
  * @param {boolean} hidden What you want the hidden attribute to be set to.
  * @param {string} id Id of the node you want to set hidden attribute in all of its children.
